e91c3c365abb4d3d7da8a3c9f0c96aa2773b7454
valery <valery.fremaux@gmail.com>
2016-11-13 00:24:31 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014090602;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2015111100;        // Requires this Moodle version.
+$plugin->requires  = 2016051900;        // Requires this Moodle version.
 $plugin->component = 'block_course_recycle'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '(3.0.0 Build(2014090600)';
+$plugin->release = '(2.8.0 Build(2014090600)';
 $plugin->maturity = MATURITY_BETA;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.0.0000';
+$plugin->codeincrement = '3.1.0000';
